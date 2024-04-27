135720f52385f1366172247960681d59a30558b4
bnelford <nelfordbc@gmail.com>
2024-04-27 16:03:05 -0600

    Initial

---
+++ rna-transcription.ts
@@ -0,0 +1,12 @@
+class Transcriptor {
+  toRna(input: string): string {
+    let rna_dict: { [name: string]: string } = {};
+     = {"G":}
+    var output = '';
+
+    return output;
+  }
+  
+}
+
+export default Transcriptor
